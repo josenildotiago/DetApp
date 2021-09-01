8cedd9ff6fc8fe042ffc40cba51b660128889d87
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-09-01 06:16:11 -0300

    updated provider name

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Text } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 import Geocoder from 'react-native-geocoding';
 
@@ -10,45 +10,45 @@ import { MapsAPI } from '../../config';
 export default () => {
     const map = useRef();
     const [mapLoc, setMapLoc] = useState({
-        center:{
+        center: {
             latitude: -5.19123792455118,
             longitude: -37.3446489953544
         },
         zoom: 16,
-        pitch:0,
-        altitude:0,
-        heading:0
+        pitch: 0,
+        altitude: 0,
+        heading: 0
     });
 
     const [fromLoc, setFromLoc] = useState({});
     const [toLoc, setToLoc] = useState({});
 
     useEffect(() => {
-        Geocoder.init(MapsAPI, {language:'pt-br'});
+        Geocoder.init(MapsAPI, { language: 'pt-br' });
         getMyCurrentPosition();
     }, [])
 
     const getMyCurrentPosition = () => {
-        Geolocation.getCurrentPosition(async (info)=>{
+        Geolocation.getCurrentPosition(async (info) => {
             const geo = await Geocoder.from(info.coords.latitude, info.coords.longitude);
             if (geo.results.length > 0) {
                 const loc = {
-                    name:geo.results[0].formatted_address,
-                    center:{
-                        latitude:info.coords.latitude,
-                        longitude:info.coords.longitude
+                    name: geo.results[0].formatted_address,
+                    center: {
+                        latitude: info.coords.latitude,
+                        longitude: info.coords.longitude
                     },
                     zoom: 16,
-                    pitch:0,
-                    altitude:0,
-                    heading:0
+                    pitch: 0,
+                    altitude: 0,
+                    heading: 0
                 };
 
                 setMapLoc(loc);
                 setFromLoc(loc);
             }
             console.log(geo.results[0]);
-        }, (error)=>{
+        }, (error) => {
             console.log(error);
         });
     }
@@ -57,9 +57,11 @@ export default () => {
         <Container>
             <MapView
                 ref={map}
-                style={{ flex:1 }}
-                provider="google"
+                style={{ flex: 1 }}
+                provider={PROVIDER_GOOGLE}
                 camera={mapLoc}
+            // showsUserLocation
+            // initialRegion={origin}
             ></MapView>
         </Container>
     );
